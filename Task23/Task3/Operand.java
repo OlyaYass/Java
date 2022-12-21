3670c088894b18ce1ad649dc095480400bdea472
OlyaYass <120303234+OlyaYass@users.noreply.github.com>
2022-12-21 13:59:50 +0300

    Add files via upload

---
+++ Task23/Task3/Operand.java
@@ -0,0 +1,9 @@
+package Task3;
+
+public abstract class Operand {
+    protected double value;
+
+    public double getValue() {
+        return value;
+    }
+}
---
+++ Task23/Task3/Tester.java
@@ -0,0 +1,27 @@
+package Task3;
+
+import java.util.Scanner;
+
+public class Tester {
+    public static void main(String[] args) {
+        System.out.println(
+                new Subtract(new Multiply(new Const(2), new Variable("x")), new Const(3)).evaluate(5)
+        );
+
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble();
+
+        System.out.println(
+                new Add(
+                        new Subtract(
+                                new Variable("x", 2),
+                                new Multiply(
+                                        new Const(2),
+                                        new Variable("x")
+                                )
+                        ),
+                        new Const(1)
+                ).evaluate(x)
+        );
+    }
+}
