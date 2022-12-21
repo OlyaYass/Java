832c2f688f7c6192f5ec09bf544f1890123ae1ba
OlyaYass <120303234+OlyaYass@users.noreply.github.com>
2022-12-21 19:55:48 +0300

    Update Students.java

--- Task9/Students.java
+++ Task9/Students.java
@@ -2,7 +2,7 @@ public class Students {
     public String name;
     public int mark;
 
-    public Students(String name, String mark) {
+    public Students(String name, int mark) {
         this.name = name;
         this.mark = mark;
     }
